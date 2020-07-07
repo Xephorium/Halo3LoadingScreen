5e9bec217d6f15772557a460369ef17825fbdbfe
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2020-07-06 20:48:28 -0500

    Update Speed

--- src/Halo3LoadingScreen.js
+++ src/Halo3LoadingScreen.js
@@ -14,7 +14,7 @@
 /*--- Global Configuration ---*/
 
 let config = {
-	SPEED: 1.17,                               // Speed of animation
+	SPEED: 1.1,                                // Speed of animation
     LENGTH_LOOP:80000,                         // Length of full animation (Final = 75000)
 	LENGTH_START_DELAY: 600,                   // Time between full canvas visibility and animation start
 	LENGTH_ASSEMBLY_DELAY: 2000,               // Time between animation start and ring assembly start
